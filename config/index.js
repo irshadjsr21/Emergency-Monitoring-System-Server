990d76b686ea1b047108572105d6864fec481740
Irshad Ansari <irshadjsr21@gmail.com>
2020-04-16 05:01:14 +0530

    [Fix] Resolved env variable error

--- config/index.js
+++ config/index.js
@@ -5,45 +5,30 @@
 
 const getEnvVariable = (key) => {
   const value = process.env[key];
-  if (!value) throw new Error(`ENVIREMENT VARIABLE '${key}' NOT SPECIFIED.`);
+  if (!value && process.env.NODE_ENV === 'production')
+    throw new Error(`ENVIREMENT VARIABLE '${key}' NOT SPECIFIED.`);
+  return value;
 };
 
-let config = {
+const config = {
   DB: {
-    USER: 'irshad',
-    PASSWORD: 'Sql@password123',
-    SCHEMA: 'ems',
-    HOST: '127.0.0.1',
-    DIALECT: 'mysql',
+    USER: getEnvVariable('DB_USER'),
+    PASSWORD: getEnvVariable('DB_PASSWORD'),
+    SCHEMA: getEnvVariable('DB_SCHEMA'),
+    HOST: getEnvVariable('DB_HOST'),
+    DIALECT: getEnvVariable('DB_DIALECT'),
   },
   JWT: {
-    SECRET: 'secret',
-    EXPIRES_IN: '30d',
+    SECRET: getEnvVariable('JWT_SECRET'),
+    EXPIRES_IN: getEnvVariable('JWT_EXPIRES_IN'),
   },
   SEND_GRID: {
-    API_KEY: '1',
+    API_KEY: getEnvVariable('SEND_GRID_API_KEY'),
   },
-  API_URL: 'http://localhost:3000',
+  API_URL: getEnvVariable('API_URL'),
   HASH: {
-    SECRET: 'secret',
+    SECRET: getEnvVariable('HASH_SECRET'),
   },
 };
-
-if (process.env.NODE_ENV == 'production') {
-  config.DB.USER = getEnvVariable('DB_USER');
-  config.DB.PASSWORD = getEnvVariable('DB_PASSWORD');
-  config.DB.SCHEMA = getEnvVariable('DB_SCHEMA');
-  config.DB.HOST = getEnvVariable('DB_HOST');
-  config.DB.DIALECT = getEnvVariable('DB_DIALECT');
-
-  config.JWT.SECRET = getEnvVariable('JWT_SECRET');
-  config.JWT.EXPIRES_IN = getEnvVariable('JWT_EXPIRES_IN');
-
-  config.SEND_GRID.API_KEY = getEnvVariable('SEND_GRID_API_KEY');
-
-  config.API_URL = getEnvVariable('API_URL');
-
-  config.HASH.SECRET = getEnvVariable('HASH_SECRET');
-}
 
 module.exports = config;
